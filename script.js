3381533da2020c9f5f92182b2f16316ee9ef1758
west0423 <167289395+west0423@users.noreply.github.com>
2024-06-05 15:54:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// JavaScript
+//console.log('Hello world!');
+
+// body の背景色を変える
+function MyFunction01() {
+    const obj = document.body;
+    obj.style.backgroundColor = '#ccc';
+  }
+
+  // ID要素 の背景色を変える
+function MyFunction02() {
+    const obj = document.getElementById('id01');
+    obj.style.backgroundColor = 'orange';
+  }
